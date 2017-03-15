1aa5bae2c5426cf54511802625e70a7038ea741e
jalbertolopez <jalbertolopez11@gmail.com>
2017-03-15 02:05:59 -0600

    Creacion del proyecto

---
+++ src/pruebaexcepciones/PruebaExcepciones.java
@@ -0,0 +1,17 @@
+
+package pruebaexcepciones;
+
+/**
+ *
+ * @author jalberto
+ */
+public class PruebaExcepciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
